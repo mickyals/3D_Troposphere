c2c32437a50192cc67e05c35987b0381dc6fb71d
Mickell Als <m.als@mail.utoronto.ca>
2025-03-14 14:30:14 -0400

    adding helper.py outline functions

---
+++ helpers.py
@@ -0,0 +1,35 @@
+def set_seed():
+    pass
+
+def get_device():
+    pass
+
+def get_logger():
+    pass
+
+def get_config():
+    pass
+
+def get_model():
+    pass
+
+def get_trainer():
+    pass
+
+def start_timer():
+    pass
+
+def end_timer():
+    pass
+
+def track_lr():
+    pass
+
+def init_weights(m):
+    pass
+
+def init_wandb():
+    pass
+
+def init_logger():
+    pass
